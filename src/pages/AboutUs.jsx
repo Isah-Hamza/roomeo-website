150594f4884eee5b7aaad218debe11600f602cfd
Isah Hamza <itshamzy@gmail.com>
2024-01-18 04:41:08 +0100

    -

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../PageHeader'
+import AboutMain from '../AboutMain'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <PageHeader title={'About Us'} />
+            <AboutMain />
+        </div>
+    )
+}
+
+export default AboutUs
