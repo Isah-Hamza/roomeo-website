b68d4539ea8c366ee2831f701e348e95517b6ae5
Isah Hamza <itshamzy@gmail.com>
2024-01-18 04:40:57 +0100

    -

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../Header'
+import Hero from '../Hero'
+import OwnProperty from '../OwnProperty'
+import Invest from '../Invest'
+import StartJourney from '../StartJourney'
+import Faq from '../Faq'
+import Review from '../Review'
+import Footer from '../Footer'
+
+const Landing = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <OwnProperty />
+            <Invest />
+            <StartJourney />
+            <Faq />
+            <Review />
+            <Footer />
+        </div>
+    )
+}
+
+export default Landing
